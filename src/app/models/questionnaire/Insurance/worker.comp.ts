f228104a9ce17cbc79a036e17cfcf3ea5928c35a
mmadel <mohammed.adel82@gmail.com>
2023-03-14 12:28:33 +0200

    desin and bind to model worker-comp component

--- src/app/models/questionnaire/Insurance/worker.comp.ts
+++ src/app/models/questionnaire/Insurance/worker.comp.ts
@@ -1,15 +1,15 @@
 export class WrokerComp {
-    injuryType: string;
+    injuryType: string ="";
     accidentDate: Date;
     accidentDateTimeStamp: number;
-    wrokerStatus: string;
+    wrokerStatus: string ='';
     address: string;
     fax: string;
     insuranceName: string;
     claimNumber: number;
     adjusterInfoName: string;
     adjusterInfoPhone: string;
-    AttorneyInfoName: string;
-    AttorneyInfoPhone: string;
+    attorneyInfoName: string;
+    attorneyInfoPhone: string;
     caseStatus: string;
 }
--- src/app/modules/patient.questionnaire/components/insurance.information/worker.comp/worker-comp.component.ts
+++ src/app/modules/patient.questionnaire/components/insurance.information/worker.comp/worker-comp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WrokerComp } from 'src/app/models/questionnaire/Insurance/worker.comp';
+import requiredFields from '../../../service/_patient.require.fields.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-worker-comp',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./worker-comp.component.css']
 })
 export class WorkerCompComponent implements OnInit {
-
+  model : WrokerComp = new WrokerComp();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  isRequiredField(name: string): boolean {
+    var field = _.find(requiredFields, { field: name })
+    return field !== undefined ? field.required : false;
+  }
 }
