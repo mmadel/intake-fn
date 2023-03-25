7851ac59609a5fe414a6c97ead8653bec5b4f4ac
mmadel <mohammed.adel82@gmail.com>
2023-03-25 23:26:39 +0200

    impl patient list service and connect to BE with

---
+++ src/app/modules/patient.admin/services/patient-list.service.ts
@@ -0,0 +1,78 @@
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, debounceTime, distinctUntilChanged, Observable, retry, switchMap, throwError } from 'rxjs';
+
+const httpOptions = {
+  // headers: new HttpHeaders({
+  //   'Content-Type': 'application/json',
+  //   'Access-Control-Allow-Origin': '*',
+  //   Connection: 'keep-alive'
+  // })
+};
+
+export interface IData {
+  number_of_records: number;
+  number_of_matching_records: number;
+  records: IUsers[];
+}
+
+export interface IUsers {
+  firstName: string;
+  middleName: string;
+  email: string;
+  country: string;
+  phoneType: string;
+  phoneNumber: string;
+  idType:string,
+  patientId:string
+}
+
+export interface IApiParams {
+  offset?: number;
+  limit?: number;
+  columnFilter?: string;
+  columnSorter?: string;
+  sort?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientListService {
+  private baseUrl = "http://localhost:8080/patient"
+  constructor(private httpClient: HttpClient) { }
+
+  getPatients(config$: BehaviorSubject<IApiParams>): Observable<any> {
+    console.log(JSON.stringify(config$));
+    return config$.pipe(
+      debounceTime(100),
+      distinctUntilChanged(
+        (previous, current) => {
+          return JSON.stringify(previous) === JSON.stringify(current);
+        }
+      ),
+      switchMap((config) => this.fetchData(config))
+    );
+  }
+  private fetchData(params: IApiParams): Observable<IData> {
+    const apiParams = {
+      ...params
+    };
+    const httpParams: HttpParams = new HttpParams({ fromObject: apiParams });
+
+    const options = Object.keys(httpParams).length
+      ? { params: httpParams, ...httpOptions }
+      : { params: {}, ...httpOptions };
+
+    return this.httpClient
+      .get<IData>(this.baseUrl, options)
+      .pipe(
+        retry({ count: 1, delay: 1000, resetOnSuccess: true }),
+        catchError(this.handleHttpError)
+      );
+  }
+  private handleHttpError(error: HttpErrorResponse) {
+    return throwError(() => error);
+  }
+
+}
