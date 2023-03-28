df8d50e23f8e50e8b625a8d10ef29477d26712a3
mmadel <mohammed.adel82@gmail.com>
2023-03-28 20:43:01 +0200

    impl export result as excel sheet

--- src/app/modules/patient.admin/components/reports/recommendation.report.component.ts
+++ src/app/modules/patient.admin/components/reports/recommendation.report.component.ts
@@ -49,12 +49,35 @@ export class RecommendationReportComponent implements OnInit {
     },
   ];
   colors = { color: 'primary', textColor: 'primary' };
-
+  public customRanges = {
+    Today: [new Date(), new Date()],
+    Yesterday: [
+      new Date(new Date().setDate(new Date().getDate() - 1)),
+      new Date(new Date().setDate(new Date().getDate() - 1))
+    ],
+    'Last 7 Days': [
+      new Date(new Date().setDate(new Date().getDate() - 6)),
+      new Date(new Date())
+    ],
+    'Last 30 Days': [
+      new Date(new Date().setDate(new Date().getDate() - 29)),
+      new Date(new Date())
+    ],
+    'This Month': [
+      new Date(new Date().setDate(1)),
+      new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
+    ],
+    'Last Month': [
+      new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1),
+      new Date(new Date().getFullYear(), new Date().getMonth(), 0)
+    ]
+  };
   ngOnInit(): void {
     this.result = {
       resultCount: 0,
       result: []
     }
+    console.log(this.result.result.length)
   }
   search() {
     this.formatDate();
@@ -85,5 +108,20 @@ export class RecommendationReportComponent implements OnInit {
       this.errorMsg = 'Please select patient Source Doctor/Entity input';
       this.searchInputNotValid = true
     }
+  }
+  exportResult() {
+    this.patientReportingService.export(this.result.result).subscribe(
+      (response) => {
+        const a = document.createElement('a')
+        const objectUrl = URL.createObjectURL(response)
+        a.href = objectUrl
+        var nameDatePart = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+        a.download = 'patient-' + nameDatePart + '.xlsx';
+        a.click();
+        URL.revokeObjectURL(objectUrl);
+      },
+      (error) => {
+        console.log(error)
+      });
   }
 }
--- src/app/modules/patient.admin/services/patient.reporting.service.ts
+++ src/app/modules/patient.admin/services/patient.reporting.service.ts
@@ -8,8 +8,8 @@ export interface IPatientResult {
   country: string;
   phoneType: string;
   phoneNumber: string;
-  idType:string,
-  patientId:string
+  idType: string,
+  patientId: string
 }
 export interface ISearchResult {
   resultCount: number;
@@ -22,9 +22,15 @@ export class PatientReportingService {
   private baseUrl = "http://localhost:8080/reports"
   constructor(private httpClient: HttpClient) { }
 
-  search(searchCriteria:PatientSearchCriteria){
+  search(searchCriteria: PatientSearchCriteria) {
     const changePatientRequiredFieldsURL = this.baseUrl + '/recommendation';
     const headers = { 'content-type': 'application/json' }
     return this.httpClient.post(changePatientRequiredFieldsURL, JSON.stringify(searchCriteria), { 'headers': headers })
+  }
+
+  export(result: IPatientResult[]) {
+    const headers = { 'content-type': 'application/json' }
+    const changePatientRequiredFieldsURL = this.baseUrl + '/generator/excel';
+    return this.httpClient.post(changePatientRequiredFieldsURL, JSON.stringify(result), { 'headers': headers, responseType: 'blob' })
   }
 }
