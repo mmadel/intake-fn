02f82bafddf9d7a238a1bf3df2de57c75375ae0e
mmadel <mohammed.adel82@gmail.com>
2023-03-29 22:24:38 +0200

    generate patient numbers

--- src/app/models/dashboard/dashboard.data.container.ts
+++ src/app/models/dashboard/dashboard.data.container.ts
@@ -2,6 +2,9 @@ import { GenderContainer } from "./gender.container";
 import { PatientSourceContainer } from "./patient.source.container";
 
 export interface DashboardDataContainer {
+    totalNumberOfPatient: number;
+    totalNumberOfCompensationNoFaultPatient: number;
+    totalNumberOfCommercialPatient: number;
     genderContainer: GenderContainer;
     patientSourceContainer: PatientSourceContainer;
 }
