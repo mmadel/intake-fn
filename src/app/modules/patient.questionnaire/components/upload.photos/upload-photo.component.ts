f1ff8ed5429abb4be840810bf41e3ddc0ce0cf03
mmadel <mohammed.adel82@gmail.com>
2023-04-02 21:31:03 +0200

    add upload file comp from id and insurance

--- src/app/modules/patient.questionnaire/components/upload.photos/upload-photo.component.ts
+++ src/app/modules/patient.questionnaire/components/upload.photos/upload-photo.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-photo.component.css']
 })
 export class UploadPhotoComponent implements OnInit {
-
+  uploadedImage: File;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onImageUpload(event:any) {
+    this.uploadedImage = event.target.files[0];
+    console.log(this.uploadedImage)
+  }
+  imageUploadAction(){
+    const imageFormData = new FormData();
+    imageFormData.append('image', this.uploadedImage, this.uploadedImage.name);
+  }
 }
