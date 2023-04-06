0ec022634c8b1153bd9490101ff93bb435e2903e
mmadel <mohammed.adel82@gmail.com>
2023-04-06 17:07:34 +0200

    fix path in prod env. file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  dashboardBaseUrl: '/dashboard/',
-  patientBaseUrl: '/patient/',
-  reportingBaseUrl: '/reports/',
-  patientAdminBaseUrl: '/patient/admin/'
+  dashboardBaseUrl: '/dashboard',
+  patientBaseUrl: '/patient',
+  reportingBaseUrl: '/reports',
+  patientAdminBaseUrl: '/patient/admin'
 };
