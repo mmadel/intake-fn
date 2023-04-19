b00ed3d3b944b2264940a9bbc956b00af13a64af
mmadel <mohammed.adel82@gmail.com>
2023-04-20 00:55:28 +0200

    fix checking patient in submition component

--- src/app/modules/patient.questionnaire/components/submition/submition.component.ts
+++ src/app/modules/patient.questionnaire/components/submition/submition.component.ts
@@ -12,7 +12,7 @@ export class SubmitionComponent implements OnInit {
   constructor(private router: Router, private localService: LocalService) { }
 
   ngOnInit(): void {
-    if (this.localService.getData('patient') !== null) {
+    if (this.localService.getData('patient') !== "") {
       this.router.navigate(['/questionnaire/add']);
     }
   }
