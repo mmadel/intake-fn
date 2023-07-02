5bedafea62cef02b297f86df5893a997117d344f
mmadel <mohammed.adel82@gmail.com>
2023-07-02 14:24:07 +0300

    undoe patient intake counter

--- src/app/modules/patient.questionnaire/components/questionnaire.add/questionnaire-add.component.ts
+++ src/app/modules/patient.questionnaire/components/questionnaire.add/questionnaire-add.component.ts
@@ -43,7 +43,7 @@ export class QuestionnaireAddComponent implements OnInit {
 
   ];
 
-  counter: number = 6;
+  counter: number = 1;
   progressValue: number = 0;
   windowScrolled: boolean = true;
   validator: ValidatorContainer;
