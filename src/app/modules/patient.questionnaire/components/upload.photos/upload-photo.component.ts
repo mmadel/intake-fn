991e93a33b6d5db1700c5a69ea4675a8040f2b75
mmadel <mohammed.adel82@gmail.com>
2023-07-02 23:33:27 +0300

    compress images before upload it

--- src/app/modules/patient.questionnaire/components/upload.photos/upload-photo.component.ts
+++ src/app/modules/patient.questionnaire/components/upload.photos/upload-photo.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PatientService } from '../../service/patient.service';
+import { NgxImageCompressService } from 'ngx-image-compress';
 
 @Component({
   selector: 'app-upload-photo',
@@ -8,7 +8,8 @@ import { PatientService } from '../../service/patient.service';
 })
 export class UploadPhotoComponent implements OnInit {
   imageFormData: FormData = new FormData();
-  constructor(private patientService: PatientService) { }
+
+  constructor(private imageCompress: NgxImageCompressService) { }
 
   ngOnInit(): void {
   }
@@ -17,22 +18,79 @@ export class UploadPhotoComponent implements OnInit {
 
     if (photoType === 'pIdfront') {
       var uploadedIDFrontImage: File = event.target.files[0];
-      this.imageUploadAction(uploadedIDFrontImage, 'pIdfront')
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+        var localUrl = event.target.result;
+        this.compressFile(localUrl, uploadedIDFrontImage['name'],'pIdfront')
+
+      }
+      reader.readAsDataURL(uploadedIDFrontImage);
     }
 
     if (photoType === 'pIdback') {
       var uploadedIDBackImage: File = event.target.files[0];
-      this.imageUploadAction(uploadedIDBackImage, 'pIdback')
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+        var localUrl = event.target.result;
+        this.compressFile(localUrl, uploadedIDBackImage['name'],'pIdback')
+
+      }
+      reader.readAsDataURL(uploadedIDBackImage);
     }
     if (photoType === 'pinsurancefront') {
       var uploadedInsuranceFrontImage: File = event.target.files[0];
-      this.imageUploadAction(uploadedInsuranceFrontImage, 'pinsurancefront')
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+        var localUrl = event.target.result;
+        this.compressFile(localUrl, uploadedInsuranceFrontImage['name'],'pinsurancefront')
+
+      }
+      reader.readAsDataURL(uploadedInsuranceFrontImage);
     }
     if (photoType === 'pinsuranceback') {
       var uploadedInsuranceBackImage: File = event.target.files[0];
-      this.imageUploadAction(uploadedInsuranceBackImage, 'pinsuranceback')
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+        var localUrl = event.target.result;
+        this.compressFile(localUrl, uploadedInsuranceBackImage['name'],'pinsuranceback')
+
+      }
+      reader.readAsDataURL(uploadedInsuranceBackImage);
     }
 
+  }
+  
+  compressFile(image: any, fileName: any , fileSuffix:string) {
+    var orientation = -1;
+    var sizeOfOriginalImage = this.imageCompress.byteCount(image) / (1024 * 1024);
+    console.warn('Size in bytes is now:', sizeOfOriginalImage);
+    this.imageCompress.compressFile(image, orientation, 50, 50).then(
+      result => {
+        var imgResultAfterCompress = result;
+        var sizeOFCompressedImage = this.imageCompress.byteCount(result) / (1024 * 1024)
+        console.warn('Size in bytes after compression:', sizeOFCompressedImage);
+        // create file from byte
+        const imageName = fileName;
+        // call method that creates a blob from dataUri
+        const imageBlob = this.dataURItoBlob(imgResultAfterCompress.split(',')[1]);
+        //imageFile created below is the new compressed file which can be send to API in form data
+        const imageFile = new File([imageBlob], imageName, {
+          type: 'image/jpeg'
+        });
+        this.imageUploadAction(imageFile,fileSuffix);
+      });
+  }
+  dataURItoBlob(dataURI: any) {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([int8Array], {
+      type: 'image/jpeg'
+    });
+    return blob;
   }
   imageUploadAction(uploadedImage: File, imageName: string) {
     this.imageFormData.append('files', uploadedImage, uploadedImage.name + ':' + imageName);
