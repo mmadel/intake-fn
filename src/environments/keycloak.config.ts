9123d1d21aa60a4d730ff6248d064c187d5f0379
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-08-14 19:19:03 +0300

    change realm and client for kc

--- src/environments/keycloak.config.ts
+++ src/environments/keycloak.config.ts
@@ -2,8 +2,8 @@ import { KeycloakConfig } from "keycloak-js";
 
 const keycloakConfig: KeycloakConfig = {
     url: 'http://localhost:8180',
-    realm: 'demo-realm',
-    clientId: 'demo-angular',
+    realm: 'patient-intake',
+    clientId: 'intake-ui',
   };
   
   export default keycloakConfig;
