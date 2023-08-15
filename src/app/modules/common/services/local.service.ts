ff482f1dbec25af577a43d80819c3367c3caadbf
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-08-15 17:38:58 +0300

    enhanced encryption methodology

--- src/app/modules/common/services/local.service.ts
+++ src/app/modules/common/services/local.service.ts
@@ -17,8 +17,14 @@ export class LocalService {
     localStorage.removeItem(key);
   }
 
-  private encrypt(txt: string): string {
-    return CryptoJS.AES.encrypt(txt, this.key).toString();
+  public encrypt(txt: string): string {
+    var hash = CryptoJS.SHA1('0123456789123456');
+    var key = CryptoJS.lib.WordArray.create(hash.words.slice(0, 16 / 4));
+    let encrypted = CryptoJS.AES.encrypt(txt, key, {
+      mode: CryptoJS.mode.ECB,
+    });
+    return encrypted.toString();
+    //return CryptoJS.AES.encrypt(txt, this.key).toString();
   }
 
   private decrypt(txtToDecrypt: string) {
