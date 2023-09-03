c83478685b28aa4819c9dbbf1c6144ef8ece493b
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-09-03 22:57:20 +0300

    fill the user and entity list

--- src/app/modules/patient.admin/components/audit/audit.component.ts
+++ src/app/modules/patient.admin/components/audit/audit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/modules/security/model/user';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-audit',
@@ -8,11 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class AuditComponent implements OnInit {
   searchInputNotValid: boolean = false;
   errorMsg: string;
-  constructor() { }
+  users: User[] = new Array();
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.get().subscribe(response => {
+
+      response.body?.forEach(element => {
+        if (element.clinics?.length !== undefined && element.clinics?.length > 0) {
+          this.users?.push(element)
+        }
+      });
+    },
+      error => {
+        console.log(error)
+      },
+    )
   }
 
-  find(){}
-  exportResult(){}
+  find() { }
+  exportResult() { }
 }
