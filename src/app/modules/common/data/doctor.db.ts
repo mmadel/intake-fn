38830d9b117cd4f18e35fe2a5afa37f9273c150a
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-10-19 17:37:47 +0300

    make doctor fields auto complete

---
+++ src/app/modules/common/data/doctor.db.ts
@@ -0,0 +1,29 @@
+import { Doctor } from "../model/doctor";
+
+export const Doctors:Doctor[]=[
+    {
+        name:'mohamed adel',
+        npi:'3030303303'
+    },
+    {
+        name:'Mahmoud ahmed',
+        npi:'9548'
+    },
+    {
+        name:'sami gamal',
+        npi:'5837434'
+    },
+    {
+        name:'khaled twafik',
+        npi:'4755855'
+    },
+    {
+        name:'eman mahmoud',
+        npi:'4949444'
+    },
+    {
+        name:'nour yasser',
+        npi:'3749844'
+    },
+
+]
---
+++ src/app/modules/common/model/doctor.ts
@@ -0,0 +1,4 @@
+export interface Doctor{
+    name?:string,
+    npi?:string;
+}
