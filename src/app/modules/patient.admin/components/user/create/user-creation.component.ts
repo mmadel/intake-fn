e3bb66ed2f43a90afc481f352a0358a464c217c8
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-07-22 16:28:18 +0300

    re-design  create user form

--- src/app/modules/patient.admin/components/user/create/user-creation.component.ts
+++ src/app/modules/patient.admin/components/user/create/user-creation.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LocalService } from 'src/app/modules/common';
-import { countries } from 'src/app/modules/common/components/address/country-data-store';
-import { Countries } from 'src/app/modules/common/components/address/model/country.model';
 import { states } from 'src/app/modules/common/components/address/state-data-store';
-import { User } from 'src/app/modules/security/model/user';
+import { noSpecialCharactersValidator } from 'src/app/modules/patient.digital.intake/components/create/validators/custom.validation/special.characters.validator';
 import { Clinic } from '../../../models/clinic.model';
 import { ClinicService } from '../../../services/clinic/clinic.service';
 import { UserService } from '../../../services/user/user.service';
+import {EmailValidator  } from 'src/app/modules/patient.digital.intake/components/create/validators/custom.validation/email.validator';
 
 interface UserRole {
   name: string;
@@ -21,30 +20,15 @@ interface UserRole {
   styleUrls: ['./user-creation.component.css']
 })
 export class UserCreationComponent implements OnInit {
-  countries: Countries[] = countries;
+  @Input() selectedUser: number
   states: string[] = states;
+  userForm: FormGroup
+  isValidForm: boolean = false;
   userRoles: UserRole[] = [
     { name: "Administrator", value: "administrator" },
     { name: "Normal User", value: "normal" }
   ]
-  errorMessage: string | null;
   returnClinics: Clinic[] = new Array();
-  @ViewChild('userCreateForm') userCreateForm: NgForm;
-  form = {
-    name: null,
-    firstName: null,
-    lastName: null,
-    email: null,
-    password: null,
-    useraddress: null,
-    useraddresscountry: null,
-    useraddressstate: null,
-    useraddressprovince: null,
-    useraddresscity: null,
-    useraddresszipcode: null,
-    userrole: null,
-    selectedClinics: null
-  };
   constructor(private clinicService: ClinicService,
     private userService: UserService,
     private router: Router,
@@ -52,6 +36,11 @@ export class UserCreationComponent implements OnInit {
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.getClinics();
+    this.createUserForm();
+  }
+
+  private getClinics() {
     this.clinicService.getActive().subscribe(response => {
       response.body?.forEach(element => {
         this.returnClinics?.push(element)
@@ -61,62 +50,42 @@ export class UserCreationComponent implements OnInit {
         console.log(error)
       },
     )
-
   }
-  create(event: any) {
-    if (event.submitter.innerHTML === ' Select all options ') {
-      return;
-    }
-    var user: User = {
-      id: null,
-      uuid: '',
-      name: this.form.name,
-      firstName: this.form.firstName,
-      lastName: this.form.lastName,
-      email: this.form.email,
-      password: this.localService.encrypt(this.form.password !== null ? this.form.password : ''),
-      address: this.createAddressModel(),
-      userRole: this.form.userrole,
-      clinics: this.createClinics(this.form.selectedClinics)
-    }
-    if (this.userCreateForm.valid) {
-      this.userService.create(user).subscribe(
-        (response) => {
-          this.router.navigateByUrl('admin/user/list')
-        },
-        (error) => {
-          console.log(error);
-          this.toastr.error(error.error.message, 'Error In Creation');
-        });
-    } else {
-      console.log('not valid')
-      this.errorMessage = 'Please enter valid data';
+  private createUserForm() {
+    const zipCodeRgx = new RegExp("^\\d{5}(?:[-\s]\\d{4})?$");
+    this.userForm = new FormGroup({
+      'firstName': new FormControl(null, [Validators.required, noSpecialCharactersValidator()]),
+      'lastName': new FormControl(null, [Validators.required, noSpecialCharactersValidator()]),
+      'email': new FormControl(null, [Validators.required, EmailValidator()]),
+      'password': new FormControl(null, [Validators.required]),
+      'first-address': new FormControl(null, [Validators.required, noSpecialCharactersValidator()]),
+      'second-address': new FormControl(null, noSpecialCharactersValidator()),
+      'city-address': new FormControl(null, [Validators.required, noSpecialCharactersValidator()]),
+      'state-address': new FormControl(null, [Validators.required]),
+      'zipcode-address': new FormControl(null, [Validators.required, Validators.min(10), Validators.pattern(zipCodeRgx)]),
+      'userRole': new FormControl(null, [Validators.required]),
+      'clinics': new FormControl(null, [Validators.required]),
+    })
+  }
+  create(){
+    if (this.userForm?.valid) {
+      this.isValidForm = false;
+    }else{
+      this.isValidForm = true;
+      this.isValidForm = true;
+      Object.keys(this.userForm.controls).forEach(field => {
+        const control = this.userForm.get(field);
+        control?.markAsTouched({ onlySelf: true });
+      });
+      this.scrollUp()
     }
   }
-  resetError() {
-
-  }
-  createAddressModel(): any {
-    return {
-      address: this.form.useraddress,
-      state: this.form.useraddressstate,
-      city: this.form.useraddresscity,
-      zipCode: this.form.useraddresszipcode
-    }
-  }
-
-  createClinics(ids: string[] | null) {
-    var clinics: Clinic[] = new Array();
-    ids?.forEach(element => {
-      var clinic: Clinic = {
-        id: Number(element),
-        name: null,
-        address: "",
-        selected: false,
-
+  private scrollUp() {
+    (function smoothscroll() {
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.scrollTo(0, 0);
       }
-      clinics.push(clinic)
-    });
-    return clinics;
+    })();
   }
 }
